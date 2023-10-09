4922185223bb7c3cdec6edc81d514cb6ea803c64
Tin Bašić <tin.basic22@gmail.com>
2023-10-09 21:18:15 +0200

    Bok bok 3

--- app/app.cpp
+++ app/app.cpp
@@ -1,12 +1,30 @@
 #include "app.h"
 
 #include <format>
+#include <cmath>
 
 namespace vsite::oop::p1 {
 
-/*
-* function implementations
-*/
+	double f1(int a, int b) {
+
+		double c = pow(a, b);
+
+		return c;
+
+
+
+	}
+
+	bool f2(bool b) {
+
+		return not b;
+
+	}
+
+	int f3(int a, int b, int c, int d) {
+
+		return a + b + c + d;
+	}
 
 
 } // namespace
--- app/app.h
+++ app/app.h
@@ -5,9 +5,10 @@
 
 namespace vsite::oop::p1 
 {
-/*  
-* function declarations
-*/
+	double f1(int a, int b);
 
+	bool f2(bool b);
+
+	int f3(int a = 1, int b = 2, int c = 3, int d =4);
 
 }
